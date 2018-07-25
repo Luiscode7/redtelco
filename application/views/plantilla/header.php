c14a76bb347e5e025dd975c1435fdcbfdb9a2516
Luis Venegas <luiseduardo.venegas7@gmail.com>
2018-07-24 23:25:15 -0400

    v1.1 navbar con iconos sin completar

--- application/views/plantilla/header.php
+++ application/views/plantilla/header.php
@@ -8,19 +8,22 @@
     <script src="<?php echo base_url();?>assest/js/jquery-3.3.1.min.js"></script>
     <script src="<?php echo base_url();?>assest/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>assest/js/datatables.min.js"></script>
+    <script defer src="<?php echo base_url();?>assest/js/all.js"></script>
     <link rel="stylesheet" href="<?php echo base_url();?>assest/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assest/css/datatables.min.css">
-    <link rel="stylesheet" href="<?php echo base_url();?>assest/css/fontawesome.min.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assest/css/normalize.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assest/css/styles_principal.css">
 </head>
 <body>
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#"><i class="fas fa-home"></i>Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+  <a class="navbar-brand" href="#">TELCO-SOCIAL</a>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#"><i class="fas fa-home"></i>Home</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-home"></i>Features</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-home"></i>Pricing</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-home"></i>Disabled</a>
     </div>
+  </div>
 </header>
