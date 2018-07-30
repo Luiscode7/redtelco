f8eb1af6155d6b93856a40f9ae3aea6386ceab10
Luis Venegas <luiseduardo.venegas7@gmail.com>
2018-07-30 17:55:39 -0400

    navbar responsive 70% terminado

--- application/views/plantilla/header2.php
+++ application/views/plantilla/header2.php
@@ -24,7 +24,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse col-md-10" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse col-md-10" id="navbarSupportedContent">
               <ul class="navbar-nav">
                 <li class="nav-item active">
                   <a class="nav-link" href="#"><i class="fas fa-home mr-2 align-baseline"></i>Inicio</a>
